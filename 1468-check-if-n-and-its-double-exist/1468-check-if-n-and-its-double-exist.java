c09d95997a64d6328b717a58993ea4ab9060b881
SAYANTAN_MONDAL_TECHI <72082049+mastersayantanmondal@users.noreply.github.com>
2024-12-12 00:39:52 +0530

    Time: 1 ms (98.36%), Space: 43.6 MB (17.48%) - LeetHub

---
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        HashSet<Integer> seen = new HashSet<>();
+        
+        for (int num : arr) {
+ 
+            if (seen.contains(2 * num) || (num % 2 == 0 && seen.contains(num / 2))) {
+                return true;
+            }
+
+            seen.add(num);
+        }
+        
+        return false;
+    }
+}
