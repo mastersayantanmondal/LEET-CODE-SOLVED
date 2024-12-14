a30f7841d8b1d1239f3a16615f54fb64cff242ea
SAYANTAN_MONDAL_TECHI <72082049+mastersayantanmondal@users.noreply.github.com>
2024-12-14 23:19:34 +0530

    Time: 2 ms (98.86%), Space: 42.2 MB (21.32%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+      Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : arr) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+     Set<Integer> freqSet = new HashSet<>();
+        for (int count : freqMap.values()) {
+            if (!freqSet.add(count)) {
+                return false;  
+            }
+        }
+        
+        return true; 
+    }
+}
