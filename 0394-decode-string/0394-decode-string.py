a2b57609104109630d9d848f2cd6a1d7ff388b53
SAYANTAN_MONDAL_TECHI <72082049+mastersayantanmondal@users.noreply.github.com>
2024-12-14 23:53:38 +0530

    Time: 0 ms (100%), Space: 12.1 MB (13.03%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def decodeString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        stack = []  
+        current_string = ""  
+        current_num = 0  
+        
+        for char in s:
+            if char.isdigit():  
+                current_num = current_num * 10 + int(char)
+            elif char == '[':  
+                stack.append((current_string, current_num))
+                current_string = ""  
+                current_num = 0  
+            elif char == ']':  
+                prev_string, repeat_times = stack.pop()
+                current_string = prev_string + current_string * repeat_times
+            else:  
+                current_string += char
+        
+        return current_string
